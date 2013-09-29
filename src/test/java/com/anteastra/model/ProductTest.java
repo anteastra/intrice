68816b4f519eb63ce80297dbd27d6dd83e577fdf
anteastra <anteastra@gmail.com>
2013-09-29 18:45:58 +0700

    first commit with mvn, junit

---
+++ src/test/java/com/anteastra/model/ProductTest.java
@@ -0,0 +1,16 @@
+package com.anteastra.model;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ProductTest {
+	
+	@Test
+    public void thisAlwaysPasses() {
+    }
+	
+	@Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+}
